810d7ea229c5f3c48cf77dce284d7a47324006e4
toneskies <tony.g.badea@gmail.com>
2025-09-14 00:18:09 -0700

    add lumpiness to asteroids

--- asteroid.py
+++ asteroid.py
@@ -6,12 +6,28 @@ from constants import *
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.rotation = 0
+        self.rotation_speed = random.uniform(-50, 50)
+
+        self.shape = []
+        for i in range(ASTEROID_VERTICES):
+            angle = (360 / ASTEROID_VERTICES) * i
+            dist = self.radius + random.uniform(-self.radius, self.radius) * ASTEROID_LUMPINESS
+            point = pygame.Vector2(0, 1).rotate(angle) * dist
+            self.shape.append(point)
+
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", (self.position.x, self.position.y), self.radius, 2 )
+        points = []
+        for point in self.shape:
+            rotated_point = point.rotate(self.rotation)
+            screen_point = rotated_point + self.position
+            points.append(screen_point)
+        pygame.draw.polygon(screen, "white", points, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+        self.rotation += self.rotation_speed * dt
         self.wrap_around_screen()
 
     def split(self):
